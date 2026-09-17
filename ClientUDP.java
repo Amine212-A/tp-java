8f03b65de1508acf467ce24e16f4d28d05ab0cfa
Your Name <amine.el-khalki@univ-rouen.fr>
2026-09-17 14:54:59 +0200

    test de connectivité UDP Client&Serveur

--- ClientUDP.java
+++ ClientUDP.java
@@ -3,22 +3,27 @@ import java.net.*;
 
 public class ClientUDP
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
-        try
-        {
-            InetAddress addr = InetAddress.getLocalHost();
-            System.out.println("adresse = " + addr.getHostName());
-            String s = "Hello World";
-            byte[] data = s.getBytes();
-            DatagramPacket packet = new DatagramPacket(data, data.length, addr, 1234);
-            DatagramSocket sock = new DatagramSocket();
-            sock.send(packet);
-            sock.close();
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
+        InetAddress addr = InetAddress.getLocalHost();
+        System.out.println("adresse=" + addr.getHostName());
+        
+        String s = "Hello World";
+        byte[] data = s.getBytes();
+        
+        DatagramPacket packet = new DatagramPacket(data, data.length, addr, 1234);
+        DatagramSocket sock = new DatagramSocket();
+        
+        sock.send(packet);
+        
+        // --- Ajout pour la Q2.3 : attendre et afficher la réponse du serveur ---
+        DatagramPacket responsePacket = new DatagramPacket(new byte[1024], 1024);
+        sock.receive(responsePacket);
+        
+        String str = new String(responsePacket.getData(), 0, responsePacket.getLength());
+        System.out.println("Message reçu du serveur : " + str);
+        
+        sock.close();
     }
 }
+
